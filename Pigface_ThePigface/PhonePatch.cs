85dc2495bfb6699fe236334303cee2eb4b123dfd
iolav <120427939+iolav@users.noreply.github.com>
2025-06-10 01:54:17 -0500

    Add project files.

---
+++ Pigface_ThePigface/PhonePatch.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace PigfaceThePigface.Patches
+{
+    [HarmonyPatch(typeof(Cellphone), "Awake")]
+    internal class PhoneAwakePatch
+    {
+        static void Postfix(Cellphone __instance)
+        {
+            var Field = typeof(Cellphone).GetField("cellPhoneObj", BindingFlags.NonPublic | BindingFlags.Instance);
+            GameObject PhoneObj = (GameObject)Field.GetValue(__instance);
+            MeshRenderer ScreenRenderer = PhoneObj.transform.Find("first_person_armature/Bone.002/Bone.001/phone_screen").GetComponent<MeshRenderer>();
+
+            Texture2D NewTexture = Plugin.Bundle.LoadAsset<Texture2D>("screen_texture");
+
+            foreach (Material Mat in ScreenRenderer.materials)
+            {
+                Mat.mainTexture = NewTexture;
+                Mat.SetTexture("_MainTex", NewTexture);
+                Mat.SetTexture("_EmissionMap", NewTexture);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Cellphone), "OpenPhone", new System.Type[] { typeof(bool) })]
+    internal class PhoneOpenPatch
+    {
+        static void Postfix(Cellphone __instance)
+        {
+            Plugin.Log!.LogInfo("hello jp!");
+
+            AudioClip NewSound = Plugin.Bundle.LoadAsset<AudioClip>("call_sound");
+
+            var Field = typeof(Cellphone).GetField("cellPhoneObj", BindingFlags.NonPublic | BindingFlags.Instance);
+            GameObject PhoneObj = (GameObject)Field.GetValue(__instance);
+
+            Transform AudioChild = PhoneObj.transform.Find("JohnPork_AudioSource");
+
+            AudioSource Audio;
+
+            if (AudioChild != null)
+            {
+                Audio = AudioChild.GetComponent<AudioSource>();
+            }
+            else
+            {
+                GameObject AudioObj = new GameObject("JohnPork_AudioSource");
+                AudioObj.transform.SetParent(PhoneObj.transform);
+                AudioObj.transform.localPosition = Vector3.zero;
+
+                Audio = AudioObj.AddComponent<AudioSource>();
+            }
+
+            Audio.clip = NewSound;
+            Audio.Play();
+        }
+    }
+}
---
+++ Pigface_ThePigface/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace PigfaceThePigface
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string PluginGUID = "iolav.Pigface_ThePigface";
+        private const string PluginName = "The Pigface";
+        private const string PluginVersion = "1.0.0";
+
+        private readonly Harmony MyHarmony = new Harmony(PluginGUID);
+
+        public static string AssetsFolderPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets");
+        public static AssetBundle Bundle = AssetBundle.LoadFromFile(Path.Combine(Plugin.AssetsFolderPath, "assets"));
+
+        public static ManualLogSource? Log;
+
+        private void Awake()
+        {
+            Log = Logger;
+            Logger.LogInfo($"Loaded {PluginGUID}");
+
+            MyHarmony.PatchAll();
+        }
+    }
+}
